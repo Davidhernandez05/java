4fc19a80f466ae9286daf831be5a4e9430891a5e
David Hernandez <davidhernandz05@gmail.com>
2025-06-25 22:35:12 -0500

    Agrego archivo de ejercicios muy basicos de java.

---
+++ HolaMundo/src/HelloWordExercises.java
@@ -0,0 +1,41 @@
+public class HelloWordExercises {
+  public static void main(String[] args) {
+    // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+    System.out.println("David");
+
+    // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+    System.out.println("Hola\nmundo");
+
+    // 3. Añade un comentario sobre lo que hace cada línea del programa.
+
+    // 4. Crea un comentario en varias líneas.
+    /*
+    * Esto es
+    * un comentario de
+    * varias líneas.
+    * */
+    // 5. Imprime tu edad, tu color favorito y tu ciudad.
+    System.out.println(28);
+    System.out.println("Negro");
+    System.out.println("Bogota");
+
+    // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+    System.err.println("Error");
+
+    // 7. Utiliza varios println para imprimir una frase.
+    System.out.print("Hola");
+    System.out.print("Mundo");
+    System.out.println("Java.");
+
+    // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+    System.out.println("  _______");
+    System.out.println(" /       \\");
+    System.out.println("|  O   O  |");
+    System.out.println("|    ^    |");
+    System.out.println("|  \\___/  |");
+    System.out.println(" \\_______/");
+
+    // 9. Intenta ejecutar el programa sin el método main y observa el error.
+    //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+  }
+}
