4dbde192ebddfff6da37344a358d7ff08d2b8286
David Hernandez <davidhernandz05@gmail.com>
2025-07-18 02:35:47 -0500

    Actualizo controladores y vistas.

--- SpringBoot/webapp/src/main/java/com/curso/webapp/webapp/controllers/PathVariableController.java
+++ SpringBoot/webapp/src/main/java/com/curso/webapp/webapp/controllers/PathVariableController.java
@@ -1,7 +1,9 @@
 package com.curso.webapp.webapp.controllers;
 
 import com.curso.webapp.webapp.models.DTO.ParamDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +35,9 @@ public class PathVariableController {
   @Value("#{${config.valuesMap}.price}")
   private Integer price;
 
-
+  //Inyectar dependencias.
+  @Autowired
+  private Environment environment;
 
   @GetMapping("/baz/{message}")
   public ParamDto baz(@PathVariable(required = true) String message) {
@@ -58,8 +62,8 @@ public class PathVariableController {
   public Map<String, Object> values(@Value("${config.code}") Integer code) { // También se pueden inyectar de esta forma directamente al método.
     Map<String, Object> json = new HashMap<>();
     json.put("Name", name);
-    json.put("Code", code);
-    json.put("Message", message);
+    json.put("Code", environment.getProperty("config.code", Integer.class));
+    json.put("Message", environment.getProperty("config.message")); //Siempre regresa un String.
     json.put("List", listOfValues);
     json.put("Productos", valuesMap);
     json.put("producto", product);
