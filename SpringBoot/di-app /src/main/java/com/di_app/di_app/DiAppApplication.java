1778821ac9127ee4cc99089fd578aec0c5db6838
David Hernandez <davidhernandz05@gmail.com>
2025-07-19 21:43:52 -0500

    Se crea estructura del proyecto sobre inyeción de dependencia.

---
+++ SpringBoot/di-app /src/main/java/com/di_app/di_app/DiAppApplication.java
@@ -0,0 +1,13 @@
+package com.di_app.di_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiAppApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/di-app /src/test/java/com/di_app/di_app/DiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.di_app.di_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
