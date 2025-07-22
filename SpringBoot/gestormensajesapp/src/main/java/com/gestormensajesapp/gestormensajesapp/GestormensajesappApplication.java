6d01a7142a934640390df393075749b0569e17c8
David Hernandez <davidhernandz05@gmail.com>
2025-07-22 16:29:27 -0500

    Creo ejercicio para DI y creo estructura del nuevo proyecto(Ejercicio).

---
+++ SpringBoot/gestormensajesapp/src/main/java/com/gestormensajesapp/gestormensajesapp/GestormensajesappApplication.java
@@ -0,0 +1,13 @@
+package com.gestormensajesapp.gestormensajesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestormensajesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestormensajesappApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/gestormensajesapp/src/test/java/com/gestormensajesapp/gestormensajesapp/GestormensajesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestormensajesapp.gestormensajesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestormensajesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
