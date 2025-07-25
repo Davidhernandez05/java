c19af259301699d8dad91d35968c077ce944bd39
David Hernandez <davidhernandz05@gmail.com>
2025-07-24 22:43:05 -0500

    Creo proyecto interceptor-app.

---
+++ SpringBoot/interceptor-app/src/main/java/com/interceptor/interceptor_app/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,6 @@
+package com.interceptor.interceptor_app.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoadingTimeInterceptor implements HandlerInterceptor {
+}
---
+++ SpringBoot/interceptor-app/src/test/java/com/interceptor/interceptor_app/InterceptorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interceptor.interceptor_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
