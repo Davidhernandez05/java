811714dfcf8bef4801d076e4892471602af3cc92
David Hernandez <davidhernandz05@gmail.com>
2025-07-28 23:47:40 -0500

    Actualizo proyecto jpa -> Crear un nuevo usuario en la BD

--- SpringBoot/jpa/src/main/java/com/jpa/jpa/JpaApplication.java
+++ SpringBoot/jpa/src/main/java/com/jpa/jpa/JpaApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @SpringBootApplication
 public class JpaApplication implements CommandLineRunner {
@@ -21,10 +23,57 @@ public class JpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		create();
+		//findOne();
+		//list();
+	}
+
+	public void findOne(){
+		//Person person = repository.findById(2L).orElseThrow();
+		//Person person = null;
+		//Optional<Person> optionalPerson = repository.findById(2L);
+
+		//if (optionalPerson.isPresent()) {
+		//	person = optionalPerson.get();
+		//	System.out.println(person);
+		//}else {
+		//	System.out.println("No se encontró al usuario con el id indicado.");
+		//}
+
+		// Otra forma pero mejor de hacer lo de arriba.
+		repository.findOne(2L).ifPresent(System.out::println);
+		repository.findName("David").ifPresent(System.out::println);
+		repository.findOneLikeName("Mig").ifPresent(System.out::println);
+		repository.findByNameContaining("res").ifPresent(System.out::println);
+		repository.findByName("Ricardo").ifPresent(System.out::println);
+
+
+	}
+
+	public void create() {
+
+		// Crear un usuario con persistencia en la BD.
+		Person person = new Person(null, "Lalo", "Thor", "PHP");
+		Person personNew = repository.save(person);
+
+		System.out.println(personNew);
+
+	}
+
+	public void list() {
+
 		//List<Person> persons = (List<Person>) repository.findAll();
 		//List<Person> persons = (List<Person>) repository.buscarProgramingLanguage("Python", "Miguel");
 		List<Person> persons = (List<Person>) repository.findByNameAndProgramingLanguage("Alejandra", "CSS");
 
+
 		persons.forEach(System.out::println);
+
+
+		List<Object[]> personValues = repository.obtenerPersonData();
+		personValues.forEach(person -> {
+			System.out.println(person[0] + " Es experto en: " + person[1]);
+		});
 	}
 }
--- SpringBoot/jpa/src/main/java/com/jpa/jpa/repositories/PersonRepository.java
+++ SpringBoot/jpa/src/main/java/com/jpa/jpa/repositories/PersonRepository.java
@@ -7,8 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+  @Query("select p from Person p where p.id=?1")
+  Optional<Person> findOne(Long id);
+
+  @Query("select p from Person p where p.name=?1")
+  Optional<Person> findName(String name);
+
+  @Query("select p from Person p where p.name like %?1%")
+  Optional<Person> findOneLikeName(String name);
 
   List<Person> findByProgramingLanguage(String programingLanguage); // Nos permite buscar por un campo, tenemos que poner findBy y el nombre del campo donde se busca.
 
@@ -16,4 +26,13 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
   List<Person> buscarProgramingLanguage(String programingLanguage, String name);
 
   List<Person> findByNameAndProgramingLanguage(String name, String programingLanguage);
+
+  @Query("select p.name, p.programingLanguage from Person p") //También se puede utilizar el where con parámetros para filtrar en caso de requerirlo.
+  List<Object[]> obtenerPersonData(); // También se puede realizar la sobrecarga de métodos en caso de requerirlo.
+
+  @Query("select p.name, p.programingLanguage from Person p where p.name=?1")
+  List<Object[]> obtenerPersonData(String name);
+
+  Optional<Person> findByName(String name);
+  Optional<Person> findByNameContaining(String name);
 }
