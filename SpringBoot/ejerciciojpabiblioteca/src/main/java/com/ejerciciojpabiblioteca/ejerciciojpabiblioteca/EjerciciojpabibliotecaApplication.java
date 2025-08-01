6fd0a4346745466b3386185318b0f97b8baa108f
David Hernandez <davidhernandz05@gmail.com>
2025-08-01 03:24:34 -0500

    Creo proyecto ejerciciojpabiblioteca

---
+++ SpringBoot/ejerciciojpabiblioteca/src/main/java/com/ejerciciojpabiblioteca/ejerciciojpabiblioteca/EjerciciojpabibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.ejerciciojpabiblioteca.ejerciciojpabiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciojpabibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciojpabibliotecaApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/ejerciciojpabiblioteca/src/test/java/com/ejerciciojpabiblioteca/ejerciciojpabiblioteca/EjerciciojpabibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejerciciojpabiblioteca.ejerciciojpabiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciojpabibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
