e62656eef13fdd77a2952becaec3a6df2a5200b5
David Hernandez <davidhernandz05@gmail.com>
2025-08-18 13:46:35 -0500

    Creamos nuevo proyecto gestor de productos.

---
+++ Ejercicios_SpringBoot/gestor_productos/src/main/java/com/gestor_productos/gestor_productos/GestorProductosApplication.java
@@ -0,0 +1,13 @@
+package com.gestor_productos.gestor_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorProductosApplication.class, args);
+	}
+
+}
