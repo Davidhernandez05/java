ea0dc8eb7cb66cb812185ecdd43549bd2cb1b5d8
David Hernandez <davidhernandz05@gmail.com>
2025-08-30 22:30:23 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos: Agrego ClienteServiceImpl

---
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/ClienteServiceImpl.java
@@ -0,0 +1,66 @@
+package com.gestor_pedidos.gestor_pedidos.services;
+
+import com.gestor_pedidos.gestor_pedidos.entities.Cliente;
+import com.gestor_pedidos.gestor_pedidos.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+  @Autowired
+  private ClienteRepository clienteRepository;
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<Cliente> findAll() {
+    return (List<Cliente>) clienteRepository.findAll();
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public ResponseEntity<?> findByClient(Integer id) {
+    Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+
+    if (optionalCliente.isPresent()) {
+      Cliente cliente = optionalCliente.get();
+      return ResponseEntity.ok().body(cliente);
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ", "No existe ningún cliente con el id: " + id));
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<?> save(Cliente cliente) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(cliente));
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<?> update(Integer id, Cliente cliente) {
+    return null;
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<?> delete(Integer id) {
+    Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+
+    if (optionalCliente.isPresent()) {
+      Cliente cliente = optionalCliente.get();
+      clienteRepository.delete(cliente);
+
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).body(Map.of("Mensaje: ", "Se elimino correctamente al cliente con id: " + id));
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ",
+        String.format("No se encontró ningún cliente con el id: %d por lo cual no es posible eliminarlo.", id)));
+  }
+}
