ddb0948201f48994a57f52f4898e5a4017add83f
David Hernandez <davidhernandz05@gmail.com>
2025-09-03 11:30:38 -0500

    Actualizo proyecto SpringBoot/primerapirest

--- SpringBoot/primerapirest/src/main/java/com/primerapirest/primerapirest/security/filter/JwtAuthenticationFilter.java
+++ SpringBoot/primerapirest/src/main/java/com/primerapirest/primerapirest/security/filter/JwtAuthenticationFilter.java
@@ -61,7 +61,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     String username = user.getUsername();
     Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
 
-    Claims claims = Jwts.claims().add("authorities", roles).build();
+    Claims claims = Jwts.claims().add("authorities", new ObjectMapper().writeValueAsString(roles)).build();
 
     String token = Jwts.builder()
         .subject(username)
