9c7ced143002d64587ac024d025fff062605ab4f
David Hernandez <davidhernandz05@gmail.com>
2025-09-06 23:21:08 -0500

    Actualizo proyecto SpringBoot/gestoPeidos -> Agrego el servicio de pedidos.

---
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/PedidosServiceImpl.java
@@ -0,0 +1,52 @@
+package com.gestor_pedidos.gestor_pedidos.services;
+
+import com.gestor_pedidos.gestor_pedidos.entities.Pedido;
+import com.gestor_pedidos.gestor_pedidos.repositories.PedidoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class PedidosServiceImpl implements PedidosService{
+
+  private PedidoRepository pedidoRepository;
+
+  @Transactional(readOnly = true)
+  @Override
+  public List<Pedido> findAll() {
+    return (List<Pedido>) pedidoRepository.findAll();
+  }
+
+  @Transactional(readOnly = true)
+  @Override
+  public ResponseEntity<?> findByPedido(Integer id) {
+
+    Optional<Pedido> optionalPedido = pedidoRepository.findById(id);
+
+    if (optionalPedido.isPresent()) {
+      Pedido pedido = optionalPedido.get();
+
+      return ResponseEntity.ok().body(pedido);
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ", "No se encontro ningun pedido con el id: " + id));
+  }
+
+  @Override
+  public ResponseEntity<?> save(Pedido pedido) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<?> update(Integer id, Pedido pedido) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<?> delete(Integer id) {
+    return null;
+  }
+}
