2cb8aa8fc41c340a27962e2b8b47582caa56fbbb
David Hernandez <davidhernandz05@gmail.com>
2025-09-10 14:52:52 -0500

    Actualizo proyecto SpringBoot/gestorpedidos

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/ProductoServiceImpl.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/ProductoServiceImpl.java
@@ -5,7 +5,6 @@ import com.gestor_pedidos.gestor_pedidos.repositories.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
