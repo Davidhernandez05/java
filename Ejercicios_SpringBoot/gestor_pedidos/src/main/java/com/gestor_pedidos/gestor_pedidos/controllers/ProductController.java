5f1cdb200891dd3cf41d8dd92a6ba2dc2e090d57
David Hernandez <davidhernandz05@gmail.com>
2025-09-11 20:50:44 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> Agrego controlador para buscar un producto por id.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/ProductController.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.gestor_pedidos.gestor_pedidos.services.ProductoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class ProductController {
   @GetMapping()
   public List<Producto> productos() {
     return productoService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findProduct(@PathVariable Integer id) {
+    return productoService.findByProduct(id);
   }
 }
