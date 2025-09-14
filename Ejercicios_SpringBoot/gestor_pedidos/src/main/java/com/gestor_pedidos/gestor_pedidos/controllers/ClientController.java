6400983bf4ab63c0b46ed77448590b0e3a74134c
David Hernandez <davidhernandz05@gmail.com>
2025-09-14 16:52:21 -0500

    Actualizo proyecto SpringBoot/Gestor_Pedidos -> Agrego función para mostrar todos los clientes en la BD.

---
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/ClientController.java
@@ -0,0 +1,31 @@
+package com.gestor_pedidos.gestor_pedidos.controllers;
+
+import com.gestor_pedidos.gestor_pedidos.entities.Cliente;
+import com.gestor_pedidos.gestor_pedidos.services.ClienteServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+  private ClienteServiceImpl clienteService;
+
+  @GetMapping
+  public ResponseEntity<?> allClient() {
+
+    List<Cliente> clienteList = clienteService.findAll();
+
+    if (clienteList.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ", "No hay ningún cliente en la base de datos."));
+    }
+
+    return ResponseEntity.ok().body(clienteList);
+  }
+}
