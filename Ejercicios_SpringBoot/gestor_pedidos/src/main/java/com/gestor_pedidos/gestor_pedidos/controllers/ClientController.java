0f654ce26b7a178b9a89e19cb0ae11799e5c9993
David Hernandez <davidhernandz05@gmail.com>
2025-09-14 20:48:40 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> Agrego función para buscar un cliente por su id.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/ClientController.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/ClientController.java
@@ -5,6 +5,7 @@ import com.gestor_pedidos.gestor_pedidos.services.ClienteServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,10 @@ public class ClientController {
     }
 
     return ResponseEntity.ok().body(clienteList);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findClientId(@PathVariable Integer id) {
+    return clienteService.findByClient(id);
   }
 }
