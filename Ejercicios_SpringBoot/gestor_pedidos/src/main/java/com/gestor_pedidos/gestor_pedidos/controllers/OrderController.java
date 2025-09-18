1fdc12b3f7627078b82b28bffb0a229941983d13
David Hernandez <davidhernandz05@gmail.com>
2025-09-18 15:43:29 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> Agrego función para buscar un pedido por id.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/OrderController.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/OrderController.java
@@ -5,6 +5,7 @@ import com.gestor_pedidos.gestor_pedidos.services.PedidosServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class OrderController {
   @GetMapping()
   public ResponseEntity<?> allOrders() {
     return pedidosService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findOrderById(@PathVariable Integer id) {
+    return pedidosService.findByPedido(id);
   }
 }
