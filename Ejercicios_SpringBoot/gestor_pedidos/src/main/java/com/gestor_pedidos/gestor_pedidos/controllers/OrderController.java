2a62a12f263dfbbdb4123e25b4790b3bbaa77ff9
David Hernandez <davidhernandz05@gmail.com>
2025-09-18 19:46:50 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> finalizo el controlador de pedido.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/OrderController.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/OrderController.java
@@ -1,13 +1,11 @@
 package com.gestor_pedidos.gestor_pedidos.controllers;
 
 
+import com.gestor_pedidos.gestor_pedidos.entities.Pedido;
 import com.gestor_pedidos.gestor_pedidos.services.PedidosServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedido")
@@ -24,5 +22,20 @@ public class OrderController {
   @GetMapping("/{id}")
   public ResponseEntity<?> findOrderById(@PathVariable Integer id) {
     return pedidosService.findByPedido(id);
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<?> saveOrder(@RequestBody Pedido pedido, @PathVariable Integer id) {
+    return pedidosService.save(pedido, id);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> updateOrder(@RequestBody Pedido pedido, @PathVariable Integer id) {
+    return pedidosService.update(id, pedido);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteOrder(@PathVariable Integer id) {
+    return pedidosService.delete(id);
   }
 }
