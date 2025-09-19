394bb1a5c4f3a9174b6187865e7641273a4b9616
David Hernandez <davidhernandz05@gmail.com>
2025-09-18 19:55:08 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesService.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesService.java
@@ -1,14 +1,18 @@
 package com.gestor_pedidos.gestor_pedidos.services;
 
 import com.gestor_pedidos.gestor_pedidos.entities.DetallesPedido;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface DetallesService {
 
-  List<DetallesPedido> findAll();
+  ResponseEntity<?> findAll();
   ResponseEntity<?> findByDetalles(Integer id);
 
+  ResponseEntity<?> saveDetails(DetallesPedido detallesPedido, Integer idPedido);
+  ResponseEntity<?> deleteDetails(Integer id);
+  ResponseEntity<?> updateDetails(DetallesPedido detallesPedido, Integer id);
 
 }
