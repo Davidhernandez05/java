3d294e0b818451cc2e12e5fafc15b33e822e65d0
David Hernandez <davidhernandz05@gmail.com>
2025-09-21 18:38:42 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> Agrego la función para buscar un detalle de pedido por id.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesServiceImpl.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -23,6 +24,7 @@ public class DetallesServiceImpl implements DetallesService{
   private PedidoRepository pedidoRepository;
 
   @Override
+  @Transactional(readOnly = true)
   public ResponseEntity<?> findAll() {
     List<DetallesPedido> detallesPedidoList = (List<DetallesPedido>) detalleRepository.findAll();
 
@@ -34,11 +36,20 @@ public class DetallesServiceImpl implements DetallesService{
   }
 
   @Override
+  @Transactional(readOnly = true)
   public ResponseEntity<?> findByDetalles(Integer id) {
-    return null;
+    Optional<DetallesPedido> detallesPedidoOptional = detalleRepository.findById(id);
+
+    if (detallesPedidoOptional.isPresent()) {
+      DetallesPedido detalles = detallesPedidoOptional.get();
+      return ResponseEntity.ok().body(detalles);
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ", "No se encontró ningún detalle de pedido con el id: " + id));
   }
 
   @Override
+  @Transactional
   public ResponseEntity<?> saveDetails(DetallesPedido detallesPedido, Integer idPedido) {
     Optional<Pedido> optionalPedido = pedidoRepository.findById(idPedido);
 
@@ -50,11 +61,13 @@ public class DetallesServiceImpl implements DetallesService{
   }
 
   @Override
+  @Transactional
   public ResponseEntity<?> deleteDetails(Integer id) {
     return null;
   }
 
   @Override
+  @Transactional
   public ResponseEntity<?> updateDetails(DetallesPedido detallesPedido, Integer id) {
     return null;
   }
