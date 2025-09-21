5301e5d25459723b6bd5e5f4a19180efe6907d33
David Hernandez <davidhernandz05@gmail.com>
2025-09-21 14:38:04 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> Creo función para guardar los detalles de un pedido.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesServiceImpl.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/services/DetallesServiceImpl.java
@@ -1,7 +1,9 @@
 package com.gestor_pedidos.gestor_pedidos.services;
 
 import com.gestor_pedidos.gestor_pedidos.entities.DetallesPedido;
+import com.gestor_pedidos.gestor_pedidos.entities.Pedido;
 import com.gestor_pedidos.gestor_pedidos.repositories.DetalleRepository;
+import com.gestor_pedidos.gestor_pedidos.repositories.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class DetallesServiceImpl implements DetallesService{
 
   @Autowired
   private DetalleRepository detalleRepository;
+
+  @Autowired
+  private PedidoRepository pedidoRepository;
 
   @Override
   public ResponseEntity<?> findAll() {
@@ -34,7 +40,13 @@ public class DetallesServiceImpl implements DetallesService{
 
   @Override
   public ResponseEntity<?> saveDetails(DetallesPedido detallesPedido, Integer idPedido) {
-    return null;
+    Optional<Pedido> optionalPedido = pedidoRepository.findById(idPedido);
+
+    if (optionalPedido.isPresent()) {
+      return ResponseEntity.ok().body(detalleRepository.save(detallesPedido));
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("Mensaje: ", "No se encontró ningún pedido con el id: " + idPedido));
   }
 
   @Override
