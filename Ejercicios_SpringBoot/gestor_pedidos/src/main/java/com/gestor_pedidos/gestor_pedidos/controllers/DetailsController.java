3e13f54d617019fb230d645d1974ce3001f0379e
David Hernandez <davidhernandz05@gmail.com>
2025-09-23 15:28:15 -0500

    Actualizo proyecto SpringBoot/gestor_pedidos -> creo función para guardar los detalles de un pedido.

--- Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/DetailsController.java
+++ Ejercicios_SpringBoot/gestor_pedidos/src/main/java/com/gestor_pedidos/gestor_pedidos/controllers/DetailsController.java
@@ -1,13 +1,11 @@
 package com.gestor_pedidos.gestor_pedidos.controllers;
 
 
+import com.gestor_pedidos.gestor_pedidos.entities.DetallesPedido;
 import com.gestor_pedidos.gestor_pedidos.services.DetallesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/details")
@@ -25,4 +23,11 @@ public class DetailsController {
   public ResponseEntity<?> findDetailsById(@PathVariable Integer id) {
     return detallesService.findByDetalles(id);
   }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<?> savedetail(@RequestBody DetallesPedido detail, @PathVariable Integer id) {
+    return detallesService.saveDetails(detail, id);
+  }
+
+  
 }
